2fc84a2dc495c03ec3a073d31be9ba0582c8bef9
Ana Rueda Guiu <ana.rueda@bosonit.com>
2023-05-30 15:46:59 +0200

    Segundo apartado con fichero application.yml

--- block5-properties/src/main/java/com/formacion/block5properties/Block5PropertiesApplication.java
+++ block5-properties/src/main/java/com/formacion/block5properties/Block5PropertiesApplication.java
@@ -1,18 +1,22 @@
 package com.formacion.block5properties;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class Block5PropertiesApplication implements CommandLineRunner {
 
+	@Autowired
+	private Environment environment;
+	private String apartadoDos;
 	@Value("${greeting}")
 	private String greeting;
 	@Value("${my.number}")
 	private int myNumber;
-
 	@Value("${new.property:new.property no tiene valor}")
 	private String newProperty;
 
@@ -25,5 +29,7 @@ public class Block5PropertiesApplication implements CommandLineRunner {
 		System.out.println("El valor de greeting es " + greeting);
 		System.out.println("El Valor de my.number es " + myNumber);
 		System.out.println("El valor de new.property es " + newProperty);
+		this.apartadoDos = environment.getProperty("greeting");
+		System.out.println(apartadoDos);
 	}
 }
