b8a686459199a88704b428e4e1379b94376c8263
Ana Rueda Guiu <ana.rueda@bosonit.com>
2023-06-06 17:34:47 +0200

    Block 6 - Simple Controllers, apartado 1 y 2

--- block6-simple-controllers/src/main/java/com/formacion/block6simplecontrollers/SimpleController.java
+++ block6-simple-controllers/src/main/java/com/formacion/block6simplecontrollers/SimpleController.java
@@ -7,14 +7,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class SimpleController {
     @GetMapping(value="/user/{nombre}")
-    public String nombre(@PathVariable String nombre) {
+    public String getName(@PathVariable String nombre) {
         return "Hola " + nombre;
     }
 
     @PostMapping(value="/useradd")
     public ResponseEntity<Person> addPerson(@RequestBody Person person) {
-        person.getNombre();
-        person.getPoblacion();
         person.setEdad(person.getEdad()+1);
 
         return ResponseEntity.ok(person);
