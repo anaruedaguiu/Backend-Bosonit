6d2a355c48cdee8fdef615ce8b4a67aca48346f3
Ana Rueda Guiu <ana.rueda@bosonit.com>
2023-06-09 11:06:06 +0200

    Block 6.3 - Person Controllers

---
+++ block6-person-controllers/src/main/java/com/example/block6personcontrollers/BeanController.java
@@ -0,0 +1,37 @@
+package com.example.block6personcontrollers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/controlador")
+public class BeanController {
+    @Autowired
+    @Qualifier("bean1")
+    Person personBean1;
+
+    @Autowired
+    @Qualifier("bean2")
+    Person personBean2;
+
+    @Autowired
+    @Qualifier("bean3")
+    Person personBean3;
+
+    @GetMapping(value="/bean/{bean}")
+    public Person getBean(@PathVariable String bean) {
+        if (bean.equals("bean1")) {
+            return personBean1;
+        } else if (bean.equals("bean2")) {
+            return personBean2;
+        } else if (bean.equals("bean3")) {
+            return personBean3;
+        } else {
+            return null;
+        }
+    }
+}
--- block6-person-controllers/src/main/java/com/example/block6personcontrollers/Block6PersonControllersApplication.java
+++ block6-person-controllers/src/main/java/com/example/block6personcontrollers/Block6PersonControllersApplication.java
@@ -2,6 +2,7 @@ package com.example.block6personcontrollers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Block6PersonControllersApplication {
@@ -10,4 +11,19 @@ public class Block6PersonControllersApplication {
 		SpringApplication.run(Block6PersonControllersApplication.class, args);
 	}
 
+	//Crear 3 objetos de tipo Persona utilizando la etiqueta @Bean
+	@Bean("bean1")
+	public Person personBean1() {
+		return new Person("PersonBean1", "LocationBean1", 1);
+	}
+
+	@Bean("bean2")
+	public Person personBean2() {
+		return new Person("PersonBean2", "LocationBean2", 2);
+	}
+
+	@Bean("bean3")
+	public Person personBean3() {
+		return new Person("PersonBean3", "LocationBean3", 3);
+	}
 }
